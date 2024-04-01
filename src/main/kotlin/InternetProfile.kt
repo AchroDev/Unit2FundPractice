67d381c44e75afb176cbb82350792a976e65c69b
AchroDev <ztblaine@protonmail.com>
2024-04-01 01:11:37 -0500

    Final Code - Compared to the solution, I only have one "println" method and one null check. Considerably conserving on code, but it would fail a lot more if the other data not being checked is null.

--- src/main/kotlin/InternetProfile.kt
+++ src/main/kotlin/InternetProfile.kt
@@ -10,5 +10,14 @@ fun main() {
 class Person(val name: String, val age: Int, val hobby: String?, val referrer: Person?) {
     fun showProfile() {
         // Fill in code
+
+        var hasReferrer: String
+
+        val checkReferrer =
+            if (referrer == null)
+                hasReferrer = "Doesn't have a referrer"
+            else hasReferrer = "Has a referrer named ${referrer.name.replaceFirstChar { it.uppercase() } } who likes to $hobby"
+
+        println("Name: $name\nAge: $age\nLikes to $hobby. $hasReferrer.\n")
     }
 }
