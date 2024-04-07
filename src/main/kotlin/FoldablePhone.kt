0a80a9edae0096fad22be9f364afa48a05dc1712
AchroDev <ztblaine@protonmail.com>
2024-04-07 11:06:10 -0500

    Switched to VScodium from IntelliJ. Added FoldablePhone class and structured the methods to detect if it is folded.

--- src/main/kotlin/FoldablePhone.kt
+++ src/main/kotlin/FoldablePhone.kt
@@ -1,14 +1,37 @@
-class Phone(var isScreenLightOn: Boolean = false){
-    fun switchOn() {
+open class Phone(var isScreenLightOn: Boolean = false){
+    
+    open fun switchOn() {
         isScreenLightOn = true
     }
 
-    fun switchOff() {
+    open fun switchOff() {
         isScreenLightOn = false
     }
 
     fun checkPhoneScreenLight() {
         val phoneScreenLight = if (isScreenLightOn) "on" else "off"
         println("The phone screen's light is $phoneScreenLight.")
+    }
+}
+
+// FoldablePhone class that inherits from the "Phone" class.
+class FoldablePhone(isFolded: Boolean) : Phone(isScreenLightOn = isFolded) {
+
+    var isFolded = true
+
+    override fun switchOn() {
+        if (isFolded == false) {
+            switchOff()
+        } else {
+            switchOn()
+        }
+    }
+
+    fun fold() {
+
+    }
+
+    fun close() {
+        
     }
 }
